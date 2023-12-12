d823287fdeebaaf21d32db7409233d14f9f064a6
Euna Kim <euna.kim@edu.sait.ca>
2023-12-12 12:39:54 -0700

    added video for live demo

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ function toggleMenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+
+document.getElementById('playButton').addEventListener('click', function () {
+    var newWindow = window.open('', 'Live Demo', 'width=800, height=600');
+
+    newWindow.document.body.innerHTML = '<video width="100%" height="100%" controls autoplay><source src="videos/taiyang clinic livedemo.mov" type="video/mp4">Your browser does not support the video tag.</video>';
+});
