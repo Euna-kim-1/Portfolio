0a853eae1dd4c95bf31a223e20b6a02b9c609d0c
Euna Kim <eunak21@gmail.com>
2024-02-07 22:42:04 -0700

    add project 5 and 6 , update some css

--- script.js
+++ script.js
@@ -40,3 +40,31 @@ document.getElementById('playButton3').addEventListener('click', function () {
 
     newWindow.document.body.innerHTML = '<video width="100%" height="100%" controls autoplay><source src="videos/turtle_crossing.mp4" type="video/mp4">Your browser does not support the video tag.</video>';
 });
+
+function toggleMenu(){
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+
+
+document.getElementById('playButton4').addEventListener('click', function () {
+    var newWindow = window.open('', 'Live Demo', 'width=800, height=600');
+
+    newWindow.document.body.innerHTML = '<video width="100%" height="100%" controls autoplay><source src="videos/us_state_game.mp4" type="video/mp4">Your browser does not support the video tag.</video>';
+});
+
+function toggleMenu(){
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+
+
+document.getElementById('playButton5').addEventListener('click', function () {
+    var newWindow = window.open('', 'Live Demo', 'width=800, height=600');
+
+    newWindow.document.body.innerHTML = '<video width="100%" height="100%" controls autoplay><source src="videos/password_manager.mp4" type="video/mp4">Your browser does not support the video tag.</video>';
+});
